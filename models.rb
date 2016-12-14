ed77bb842179fbd1713cfd8569164849ce19f85f
Alex Wasson <wasson.alex@gmail.com>
2016-12-14 12:47:41 -0500

    follower/following join table working

--- models.rb
+++ models.rb
@@ -1,11 +1,21 @@
-class User < ActiveRecord::Base
-end
+
 
 class Post < ActiveRecord::Base
+	belongs_to :user
 end
 
 class Profile < ActiveRecord::Base
+	belongs_to :user
 end
 
 class UserFollower < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :follower, :class_name => 'User'
+end
+
+class User < ActiveRecord::Base
+	has_one :profile
+	has_many :posts
+	has_many :followers, :class_name => 'UserFollower', :foreign_key => 'user_id'
+	has_many :followings, :class_name => 'UserFollower', :foreign_key => 'follower_id'
 end
